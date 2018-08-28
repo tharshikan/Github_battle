436fd9cd2af2c73af01cfdd0f89b007aa53e0752
Tharshikan <santhar2008@gmail.com>
2018-08-28 02:29:47 -0400

    FriendsNotFriends( practising map and filter)

---
+++ app/FriendsNotFriendsContainer.js
@@ -0,0 +1,23 @@
+/* @flow */
+import React,{Component} from 'react';
+
+class FriendsNotFriendsContainer extends Component<{users:Array<{name:string, isFriend:boolean}>}>{
+    render(){
+        let friends = this.props.users.filter(x=> x.isFriend==true);
+        let notFriends = this.props.users.filter(x=> x.isFriend==false);
+        return (
+        <div>
+            <h1>Friends vs NotFriends</h1>
+            <h2>Friends</h2>
+            <ul>
+                {friends.map(x => (<li>{x.name}</li>))}
+            </ul>
+            <h2>Not Friends</h2>
+            <ul>
+                {notFriends.map(x=> (<li>{x.name}</li>))}
+            </ul>
+        </div>)
+    }
+}
+
+export default FriendsNotFriendsContainer;
--- app/index.js
+++ app/index.js
@@ -3,19 +3,20 @@
 import React,{Component} from 'react';
 import ReactDOM from "react-dom";
 import Badge from './Badge.js'
+import FriendsNotFriendsContainer from './FriendsNotFriendsContainer'
 import type {User} from './Badge';
 
 
-class App extends Component<{name:number, user: User}>{
+class App extends Component<{name:number, user: User, people:Array<{name:string, isFriend:boolean}>}>{
 
     render(){
         return (
             <div>
                 <div>
-            Hello World Fist Program {this.props.name}
-
-            </div>
-            <Badge user={this.props.user}/>
+                    Hello World Fist Program {this.props.name}
+                </div>
+                <Badge user={this.props.user}/>
+                <FriendsNotFriendsContainer users={this.props.people}/>
             </div>
             
         )
@@ -28,9 +29,16 @@ let Jhon ={
     name:'Jhon Smith',
     age:36
 }
+let Aliases =[
+    {name: "Tharshikan", isFriend: false},
+    {name: "Jhon", isFriend: true},
+    {name: "Andrew", isFriend: false},
+    {name: "Mackenzie", isFriend: true},
+    {name: "Harvey", isFriend: false}
+]
 
 ReactDOM.render(
-    <App  name={5} user={Jhon}/>,
+    <App  name={5} user={Jhon} people={Aliases}/>,
     (document.getElementById('app'):any)
   );
 
