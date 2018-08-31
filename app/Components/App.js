21ba9ad91966ae3e4d650e5c9d18436707a4c3ba
Tharshikan <santhar2008@gmail.com>
2018-08-30 21:34:26 -0400

    added Components folder

--- app/Components/App.js
+++ app/Components/App.js
@@ -2,15 +2,20 @@ import React, {Component} from "react";
 import type {User} from "./Badge";
 import Badge from "./Badge";
 import FriendsNotFriendsContainer from "./FriendsNotFriendsContainer";
+import * as PropTypes from "prop-types";
+
+function HelloW(props: { name:number }) {
+    return <div>
+        Hello World Fist Prograam {props.name}
+    </div>;
+}
 
 export class App extends Component<{ name: number, user: User, people: Array<{ name: string, isFriend: boolean }> }> {
 
     render() {
         return (
             <div>
-                <div>
-                    Hello World Fist Program {this.props.name}
-                </div>
+                <HelloW name={this.props.name}/>
                 <Badge user={this.props.user}/>
                 <FriendsNotFriendsContainer users={this.props.people}/>
             </div>
--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 /* @flow */
 import React from 'react';
 import ReactDOM from "react-dom";
-import {App} from "./App";
+import {App} from "./Components/App";
 
 
 let Jhon ={
